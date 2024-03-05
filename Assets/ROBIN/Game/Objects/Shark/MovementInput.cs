ea9107647f1c19e4f8f2e14bcb6fd2d216c88aa4
Robin Pound <54936470+robinpound@users.noreply.github.com>
2024-03-05 22:47:13 +0000

    IDK dude

--- Assets/ROBIN/Game/Objects/Shark/MovementInput.cs
+++ Assets/ROBIN/Game/Objects/Shark/MovementInput.cs
@@ -3,19 +3,42 @@ using UnityEngine.InputSystem;
 
 public class MovementInput : MonoBehaviour
 {
-    public InputAction movementControls;
+    public InputAction LeftStick;
+    public InputAction RightStick;
+    public InputAction LT;
+    public InputAction LB;
+    public InputAction RT;
+    public InputAction RB;
+    public InputAction BButton;
+    public InputAction YButton;
+    public InputAction XButton;
+    public InputAction AButton;
 
-    private void OnEnable() {
-        Debug.Log("Enabled");
-        movementControls.Enable();  
+    public void OnEnable() {
+        LeftStick.Enable();  
+        RightStick.Enable();  
+        LT.Enable();  LB.Enable();  
+        RT.Enable();  RB.Enable();  
+        BButton.Enable();  
+        YButton.Enable(); 
+        XButton.Enable();  
+        AButton.Enable();  
+        
     }
-    private void OnDisable() {
-        Debug.Log("Disabled");
-        movementControls.Disable();
+    public void OnDisable() {
+        LeftStick.Disable();  
+        RightStick.Disable();  
+        LT.Disable();  LB.Disable();  
+        RT.Disable();  RB.Disable();  
+        BButton.Disable();  
+        YButton.Disable(); 
+        XButton.Disable();  
+        AButton.Disable();
     }
-    void FixedUpdate()
+    public void FixedUpdate()
     {
-        Debug.Log(movementControls.actionMap.actions);
-        //transform.position += new Vector3(movementControls.ReadValue<Vector2>().x, 0f, movementControls.ReadValue<Vector2>().y) * 0.2f;
+        Debug.Log(RightStick.ReadValue<Vector2>());
+        transform.position += new Vector3(LeftStick.ReadValue<Vector2>().x, 0f, LeftStick.ReadValue<Vector2>().y) * 0.2f;
+        transform.position += new Vector3(RightStick.ReadValue<Vector2>().x, 0f, RightStick.ReadValue<Vector2>().y) * 0.2f;
     }
 }
