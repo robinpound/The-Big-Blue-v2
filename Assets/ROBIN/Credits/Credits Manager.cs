b8f45937ecb0e89b9e7c4c2f24d11233a494eb9f
Robin Pound <54936470+robinpound@users.noreply.github.com>
2024-03-05 17:13:45 +0000

    Input Controls for credit scene

--- Assets/ROBIN/Credits/Credits Manager.cs
+++ Assets/ROBIN/Credits/Credits Manager.cs
@@ -1,9 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class CreditsManager : MonoBehaviour
 {
-  
+    public InputAction controls;
+    
+    private void OnEnable() {
+        Debug.Log("Enabled");
+        controls.Enable();  
+    }
+    private void OnDisable() {
+        Debug.Log("Disabled");
+        controls.Disable();
+    }
+    public void FixedUpdate() {
+        if(controls.triggered) {
+            Debug.Log("Pressed");
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
 }
