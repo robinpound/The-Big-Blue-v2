eee0a57e05f59566d2318b49ce3759b83d5ee9cf
Robin Pound <54936470+robinpound@users.noreply.github.com>
2024-03-13 20:41:17 +0000

    FMOD integration started

---
+++ Assets/ROBIN/Game/Objects/Landmark/Landmark.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landmark : MonoBehaviour
+{
+    // public IntroductionRange 
+
+    public void IntroduceLandmark(){
+        Debug.Log("Play cutscene");
+    }
+
+    public void UncoverLandmark(){
+        Debug.Log("Play Uncovering");
+    }
+}
